3df829bba3c819a575a58aec2c895d6707991437
ismailomodara <omodara145@gmail.com>
2023-09-15 09:39:18 +0100

    chore: dist deploy test

---
+++ dist/assets/BlankLayout-99b5f251.js
@@ -0,0 +1,1 @@
+import{_ as t,d as n,r as o}from"./index-843d45a6.js";const a=n({name:"BlankLayout"});function r(e,s,c,d,f,p){return o(e.$slots,"default")}const u=t(a,[["render",r]]);export{u as default};
